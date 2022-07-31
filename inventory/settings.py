4c7d567162275b03933f97e484303d367518da6c
AhmedRiyal <ahmedriyal@gmail.com>
2022-07-31 11:57:44 +0600

    settings changed

--- inventory/settings.py
+++ inventory/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'k2z9po4i#n+1p(^ny1el2c!om(^-l+_%&ob0azk0-ike*-)81e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['impetus-inventory.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
